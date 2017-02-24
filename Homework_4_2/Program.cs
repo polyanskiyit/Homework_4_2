c6a17266af21579af4a0fa583853bca4f8e8c363
polyanskiyit <polyanskiyit@gmail.com>
2017-02-24 23:48:33 +0200

    2

--- Homework_4_2/Program.cs
+++ Homework_4_2/Program.cs
@@ -9,36 +9,27 @@ namespace Homework_4_2
     class Program
     {
 
-        private static void m1(string v1, int v2, int v3)
+        private static void m1(string str, params int[] values)
         {
-            if ((v2 + v3) > 20)
+            int sum = 0;
+            for (int i = 0; i < values.Length; i++)
             {
-                Console.WriteLine(v1 + " sum: " + (v2 + v3));
+                sum += values[i];
             }
-            else
-            {
-                Console.WriteLine("Sorry but sum = " + (v2 + v3));
-            }
-        }
 
-        private static void m1(string v1, int v2, int v3, int v4)
-        {
-            if ((v2 + v3 + v4) > 20)
+            if (sum > 20)
             {
-                Console.WriteLine(v1 + " sum: " + (v2 + v3 + v4));
+                Console.WriteLine(str + " Sum = " + (sum));
             }
             else
             {
-                Console.WriteLine("Sorry but sum = " + (v2 + v3 + v4));
+                Console.WriteLine("Sorry but sum = " + (sum));
             }
         }
 
         static void Main(string[] args)
         {
-            m1("m1__1", 10, 5, 2);
-            m1("m1__2", 5, 5, 8);
-            m1("m1__3", 10, 9, 2);
-            m1("m1__4", 20, 9);
+            m1("string", 2, 9, 5, 1, 1, 1, 1);
             Console.Read();
         }
     }
